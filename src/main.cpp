be3163e9ba36263827f745cdd828f2488f0d6bc4
nickelmagnet <nickelmagnet@gmail.com>
2026-07-25 17:36:34 +0530

    add GLEW

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
-#include "GLFW/glfw3.h"
-#include <iostream>
+#include <GLFW/glfw3.h>
 
 int main() {
-	int a = glfwInit();
-	std::cout << a << std::endl;
-	std::cin.get();
+	if (!glfwInit()) {
+		return -1;
+	}
+	GLFWwindow* window = glfwCreateWindow(720, 720, "Landi Thassi", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5, -0.25);
+		glVertex2f(0.5, -0.25);
+		glVertex2f(0.0, 0.5);
+	 glEnd();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
 }
