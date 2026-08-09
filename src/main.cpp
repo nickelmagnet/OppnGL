fa70ec179ce60ce473e3149656317591db7832f2
nickelmagnet <nickelmagnet@gmail.com>
2026-08-09 17:08:57 +0530

    Switch to modern OpenGL with VBOs and shaders
    Replaced deprecated immediate mode rendering with VBOs and GLSL shaders. Added shader compilation and program creation utilities. Vertex data is now uploaded to GPU buffers and rendered using glDrawArrays. Removed all glBegin/glEnd and related legacy functions. OpenGL version is now printed at startup.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,38 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
+static int CompileShader(unsigned int type, const std::string& source) {
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE) {
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
+		std::cout << message << std::endl;
+		glDeleteShader(id);
+		return 0;
+	}
+	return id;
+}
+static unsigned int CreateShader(const std::string& VertexShader, const std::string& FragmentShader) {
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, VertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, FragmentShader);
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+	return program;
+}
 int main() {
 	if (!glfwInit()) {
 		return -1;
@@ -17,25 +49,51 @@ int main() {
 	if (glewInit() != GLEW_OK) {
 		return -1;
 	}
-	int positions[6] = {
-		-0.5, -0.25,
-		0.5, -0.25,
-		0.0, 0.5
-	};
 
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	float positions[6] = {
+		-0.5f, -0.25f,
+		0.5f, -0.25f,
+		0.0f, 0.5f
+	};
 
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+
+	std::string vertexShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) in vec4 position;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = position;\n"
+		"}\n";
+	std::string fragmentShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) out vec4 color;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	color = vec4(1.0, 0.0, 1.0, 1.0);\n"
+		"}\n";
+	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+ 
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glUseProgram(shader);
+
 	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5, -0.25);
-		glVertex2f(0.5, -0.25);
-		glVertex2f(0.0, 0.5);
-	 glEnd();
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
