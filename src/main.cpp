a4206c1d7848e6b375f022cbb3004c476aa4a8ef
nickelmagnet <nickelmagnet@gmail.com>
2026-09-12 17:21:35 +0530

    Abstract shader class

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,13 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
 
 #include "renderer.h"
 #include "indexbuffer.h"
 #include "vertexbuffer.h"
 #include "vertexarray.h"
+#include "shader.h"
 
-struct ShaderProgramSource {
-	std::string VertexSource;
-	std::string FragmentSource;
-};
-
-static ShaderProgramSource ParseShader(const std::string& filepath) {
-	std::ifstream stream(filepath);
-
-	enum class ShaderType {
-		NONE = -1, VERTEX = 0, FRAGMENT = 1,
-	};
-
-	ShaderType type = ShaderType::NONE;
-
-	std::string line;
-	std::stringstream ss[2];
-	while (getline(stream, line)) {
-		if (line.find("#shader") != std::string::npos) {
-			if (line.find("vertex") != std::string::npos) {
-				type = ShaderType::VERTEX;
-			}
-			else if (line.find("fragment") != std::string::npos) {
-				type = ShaderType::FRAGMENT;
-			}
-		}
-		else {
-			ss[(int)type] << line << '\n';
-		}
-	}
-	return { ss[0].str(), ss[1].str() };
-}
-
-static int CompileShader(unsigned int type, const std::string& source) {
-	unsigned int id = glCreateShader(type);
-	const char* src = source.c_str();
-	GLCall(glShaderSource(id, 1, &src, nullptr));
-	GLCall(glCompileShader(id));
-	int result;
-	GLCall(glGetShaderiv(id, GL_COMPILE_STATUS, &result));
-	if (result == GL_FALSE) {
-		int length;
-		GLCall(glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length));
-		char* message = (char*)alloca(length * sizeof(char));
-		GLCall(glGetShaderInfoLog(id, length, &length, message));
-		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
-		std::cout << message << std::endl;
-		GLCall(glDeleteShader(id));
-		return 0;
-	}
-	return id;
-}
-static unsigned int CreateShader(const std::string& VertexShader, const std::string& FragmentShader) {
-	unsigned int program = glCreateProgram();
-	unsigned int vs = CompileShader(GL_VERTEX_SHADER, VertexShader);
-	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, FragmentShader);
-	GLCall(glAttachShader(program, vs));
-	GLCall(glAttachShader(program, fs));
-	GLCall(glLinkProgram(program));
-	GLCall(glValidateProgram(program));
-	GLCall(glDeleteShader(vs));
-	GLCall(glDeleteShader(fs));
-	return program;
-}
 int main() {
 	if (!glfwInit()) {
 		return -1;
@@ -108,10 +43,6 @@ int main() {
 		0,1,2,
 		2,3,0,
 	};
-	
-	unsigned int vao;
-	GLCall(glGenVertexArrays(1, &vao));
-	GLCall(glBindVertexArray(vao));
 
 	VertexArray va;
 	VertexBuffer vb(positions, 4 * 2 * sizeof(float));
@@ -121,20 +52,15 @@ int main() {
 
 	IndexBuffer ib(indices, 6);
 
-	ShaderProgramSource source = ParseShader("res/shaders/basic.shader");
+	Shader shader("res/shaders/basic.shader");
+	shader.Bind();
 
-	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+	shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
 
-	GLCall(glUseProgram(shader));
-
-	GLCall(int location = glGetUniformLocation(shader, "u_Color"));
-	ASSERT(location != -1);
-	glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f);
-
-	GLCall(glBindVertexArray(0));
-	GLCall(glUseProgram(0));
-	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
-	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+	va.Unbind();
+	vb.Unbind();
+	ib.Unbind();
+	shader.Unbind();
 
 	float r = 0,g=1.0f,b=0.5f;
 	float incr = 0.05f,incb = 0.05f,incg = -0.05f;
@@ -142,15 +68,14 @@ int main() {
 	while (!glfwWindowShouldClose(window)) {
 		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
-		GLCall(glUseProgram(shader));
-		glUniform4f(location, r, g, b, 1.0f);
+		shader.Bind();
+		shader.SetUniform4f("u_Color", r, g, b, 1.0f);
 		
 		va.Bind();
 		ib.Bind();
 
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
-		
 		r += incr;
 		g += incg;
 		b += incb;
@@ -167,7 +92,6 @@ int main() {
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-	glDeleteProgram(shader);
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
